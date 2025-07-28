7d831dccd0708c75545047a5655da25389df1fbf
Luz Maria Esperanza Turner Benites <esperanzaturnerbenites@gmail.com>
2025-07-28 18:21:20 -0500

    minor change

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,11 @@ app.include_router(api_router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://rutadelsaborgirardot.netlify.app",
+        "https://ruta-sabor-girardot-dev.loca.lt"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
