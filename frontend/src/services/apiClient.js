8921cd9506ed7b594fc5596197cd9f99f2779119
Luz Maria Esperanza Turner Benites <esperanzaturnerbenites@gmail.com>
2025-07-30 08:23:20 -0500

    minos changes

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -4,7 +4,9 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   // timeout: 5000,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'bypass-tunnel-reminder': 'true',
+    'User-Agent': 'axios'
   }
 })
 
